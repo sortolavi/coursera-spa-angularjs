ce73b20d3b2da9b2b140b280b7afcec64fa5188d
Timo Sorakivi <timo.sorakivi@gmail.com>
2016-10-30 17:11:25 +0200

    ready

--- assignment03-solution/app.js
+++ assignment03-solution/app.js
@@ -13,6 +13,7 @@
             templateUrl: 'founditems.html',
             scope: {
                 found: '<',
+                notice: '@',
                 // myTitle: '@title',
                 onRemove: '&'
             },
@@ -27,17 +28,7 @@
 
     function FoundItemsDirectiveController() {
         var menu = this;
-
-        // list.itemsInList = function() {
-        //     for (var i = 0; i < list.items.length; i++) {
-        //         var name = list.items[i].name;
-        //         if (name.toLowerCase().indexOf("cookie") !== -1) {
-        //             return true;
-        //         }
-        //     }
-
-        //     return false;
-        // };
+       
     }
 
 
@@ -45,27 +36,21 @@
     NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(MenuSearchService) {
         var menu = this;
-        menu.found = [];
-        // menu.found = [
-        //     {name:"aku ankka"},
-        //     {name:"roope ankka"}
-        // ];
-
+        menu.notice = "";
+        
         menu.getMatchedMenuItems = function(searchTerm) {
             var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
-            // console.log(promise);
+            
             promise.then(function (response) {
-              console.log(response);
-              menu.found = response;
+                menu.found = response;
+                menu.notice = (menu.found.length === 0 )? "Nothing found!" : "";
             })
             .catch(function (error) {
-              console.log(error);
+                console.log(error);
             });
         };
 
         menu.removeItem = function(itemIndex) {
-            // console.log("'this' is: ", this);
-            // MenuSearchService.removeItem(itemIndex);
             menu.found.splice(itemIndex, 1);
         };
 
@@ -86,37 +71,17 @@
             }).then(function(result) {
                 
                 items = result.data.menu_items;
-                
-                // for(var i = 0; i < items.length; i++) {
-                //     if(items[i].description.indexOf(searchTerm) > -1) {
-                //         // console.log('found '+searchTerm+' in ' + items[i].name);
-                //         foundItems.push(items[i]);
-                //     }
-                // }
 
-                foundItems = items.filter(function(item) { return item.description.indexOf(searchTerm) > -1; });
-                
-                // result.data.menu_items is an array of objects
-                /*    
-                {
-                  description: "chicken broth with egg drop"
-                  id: 878
-                  large_portion_name: "quart"
-                  name: "Egg Drop Soup"
-                  price_large: 4.5
-                  price_small: 2.25
-                  short_name: "A2"
-                  small_portion_name: "pint"
-                }
-                */
+                foundItems = items.filter(function(item) { 
+                    var desc = item.description.toLowerCase();
+                    var s = (undefined != searchTerm)?searchTerm.toLowerCase() : undefined;
+                    return desc.indexOf(s) > -1;
+                });
 
+                if(searchTerm == '') foundItems = [];
                 return foundItems;
             });
         };
-
-        // service.removeItem = function(itemIndex) {
-        //     foundItems.splice(itemIndex, 1);
-        // };
 
 
     }
